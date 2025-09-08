b76ded4f2a7aa99c2ebd5ead466c36ec160dfae2
pvimalacode <pvimala.mail@gmail.com>
2025-09-08 10:50:06 -0400

    sql files

---
+++ consequetive_numbers.sql
@@ -0,0 +1,60 @@
+-- 180. Consecutive Numbers
+-- Solved
+-- Medium
+-- Topics
+-- premium lock icon
+-- Companies
+-- SQL Schema
+-- Pandas Schema
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column starting from 1.
+ 
+
+-- Find all numbers that appear at least three times consecutively.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
+with cte as (select id, num,
+id - row_number() over(partition by num order by id) as label
+from logs
+order by id)
+
+select distinct num as ConsecutiveNums 
+from cte
+group by label,num
+having count(label)>=3
